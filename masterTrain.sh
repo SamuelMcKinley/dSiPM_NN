d2ecdfe7c9c885d656155367484d714e9b0e3f63
Samuel McKinley <sjmck17@gmail.com>
2025-09-12 18:33:49 -0500

    Add comments

--- masterTrain.sh
+++ masterTrain.sh
@@ -18,18 +18,20 @@ gen_script() {
 #SBATCH -c 2
 #SBATCH --mem-per-cpu=16G
 
-# Edit Parameters here:
+################ Edit Parameters here: ################
 particle="pi+"
 # Energy in GeV
-Lower_E=5
-Upper_E=10
-Step_Size=5
+Lower_Energy_Bound=5
+Upper_Energy_Bound=10
+Energy_Step_Size=5
 # Amount of Events
 Events=10
-# Batch Size (Recommended <= 500 jobs maximum)
+# Batch Size (Keep Events divisible by Batch Size. Recommended <= 500 jobs maximum)
 Batch_Size=5
 #Expand array as needed
 SPAD_Sizes=("4000x4000" "2000x2000")
+
+
 
 temp_dir=/lustre/scratch/$USER/dSiPM_NN
 mkdir -p ${temp_dir}
@@ -37,13 +39,12 @@ mkdir -p ${temp_dir}/Simulation_check
 mkdir -p ${temp_dir}/tensorMaker_check
 mkdir -p ${temp_dir}/NNTraining_check
 mkdir -p ${temp_dir}/tensfold
-
 cd ..
-
 home_dir=$PWD
 
-echo "Training neural network with particle ${particle} from ${Lower_E}GeV to \
-${Upper_E}GeV in steps of ${Step_Size}GeV. ${Events} events per energy."
+echo "Training neural network with particle ${particle} from ${Lower_Energy_Bound}GeV to \
+${Upper_Energy_Bound}GeV in steps of ${Energy_Step_Size}GeV. ${Events} events per energy."
+
 
 # Check if Events divisible by specified batch size
 if (( Events % Batch_Size == 0 )); then
@@ -53,230 +54,231 @@ else
     exit 0
 fi
 
-# Start the files
+
+# Start the main scripts
 ./Simulations.sh ${Batch_Size}
 ./trainNN.sh
 ./mass_tensorMaker.sh ${Batch_Size}
 
 
-# Loop over SPAD Sizes, Energies, and Groups (Group = Total Events / Batch Size)
+# Loop over SPAD Sizes, Energies, and Groups (group = Total Events / Batch Size)
 for s in "${SPAD_Sizes[@]}"; do
-for (( i=${Lower_E}; i<=${Upper_E}; i+=${Step_Size} )); do
-for j in $(seq 1 $group); do
+  for (( i=${Lower_Energy_Bound}; i<=${Upper_Energy_Bound}; i+=${Energy_Step_Size} )); do
+    for j in $(seq 1 $group); do
 
 
-# If it's the first event, start the simulations and tensor making right away
-if [[ "$j" == 1 ]]; then
+      # If it's the first event, start the simulations and tensor making right away
+      if [[ "$j" == 1 ]]; then
 
-  echo "Starting first batch of simulations"
+        echo "Starting first training batch"
 
 
-  # Make text files to communicate with Simulation.sh jobs.
-  for ((n = 0; n < Batch_Size; n++)); do
+        # Make text files to communicate with Simulation.sh and start first batch
+        for ((n = 0; n < Batch_Size; n++)); do
 
-    cat > start_Simulations_${n}.txt <<EOT
+          cat > start_Simulations_${n}.txt <<EOT
 particle=${particle}
 energy=${i}
 group=${j}
 SPAD_Size=${s}
 EOT
 
-    mv start_Simulations_${n}.txt ${temp_dir}/
+          mv start_Simulations_${n}.txt ${temp_dir}/
 
-  done
+        done
 
-  echo "Simulation jobs started"
-
-
-  # Wait for Simulation to finish
-  while true; do
-    done_count=$(ls ${temp_dir}/Simulation_check/*.done 2>/dev/null | wc -l)
-    if [ "$done_count" -eq "${Batch_Size}" ]; then
-      rm -rf ${temp_dir}/Simulation_check/*.done
-      break
-    fi
-      sleep 1
-  done
-
-  echo "Simulation jobs finished"
+        echo "Simulation jobs started"
 
 
-  echo "Making tensors"
+        # Wait for simulations to finish
+        while true; do
+          done_count=$(ls ${temp_dir}/Simulation_check/*.done 2>/dev/null | wc -l)
+          if [ "$done_count" -eq "${Batch_Size}" ]; then
+            rm -rf ${temp_dir}/Simulation_check/*.done
+            echo "Simulation jobs finished"
+            break
+          fi
+            sleep 1
+        done
 
-  # Make text files to communicate with mass_tensorMaker.sh jobs
 
-  for ((n = 0; n < Batch_Size; n++)); do
 
-    cat > start_tensorMaker_${n}.txt <<EOT
+        # Make text files to communicate with mass_tensorMaker.sh and start first batch
+        for ((n = 0; n < Batch_Size; n++)); do
+
+          cat > start_tensorMaker_${n}.txt <<EOT
 particle=${particle}
 energy=${i}
 group=${j}
 SPAD_Size=${s}
 EOT
 
-    mv start_tensorMaker_${n}.txt ${temp_dir}/
+          mv start_tensorMaker_${n}.txt ${temp_dir}/
 
-  done
+        done
 
-  echo "Tensor jobs started"
-
-
-  # Wait for tensor making to finish
-  while true; do
-    done_count=$(ls ${temp_dir}/tensorMaker_check/*.done 2>/dev/null | wc -l)
-    if [ "$done_count" -eq "${Batch_Size}" ]; then
-      rm -rf ${temp_dir}/tensorMaker_check/*.done
-      break
-    fi
-      sleep 1
-  done
-
-  echo "Tensor jobs finished"
+        echo "Tensor jobs started"
 
 
-
-fi
+        # Wait for tensor making to finish
+        while true; do
+          done_count=$(ls ${temp_dir}/tensorMaker_check/*.done 2>/dev/null | wc -l)
+          if [ "$done_count" -eq "${Batch_Size}" ]; then
+            rm -rf ${temp_dir}/tensorMaker_check/*.done
+            echo "Tensor jobs finished"
+            break
+          fi
+            sleep 1
+        done
 
 
 
-echo "Starting training scripts"
 
-#Copying and starting trainNN.sh through text file communication
-cat > start_training.txt <<EOT
+      fi
+
+
+
+
+      # Make text files to communicate with trainNN.sh and start training
+      cat > start_training.txt <<EOT
 particle=${particle}
 energy=${i}
 group=${j}
 SPAD_Size=${s}
 EOT
 
-mv start_training.txt ${temp_dir}/
+      mv start_training.txt ${temp_dir}/
+
+      echo "Training job started"
+
+      if [[ "$j" == "${group}" ]]; then
+
+        # Waiting for training to finish
+        while true; do
+          done_count=$(ls ${temp_dir}/NNTraining_check/*.done 2>/dev/null | wc -l)
+          if [ "$done_count" -eq 1 ]; then
+            rm -rf ${temp_dir}/NNTraining_check/*.done
+            echo "Training job finished"
+            break
+          fi
+            sleep 1
+        done
 
 
-if [[ "$j" == "${group}" ]]; then
-
-  while true; do
-    done_count=$(ls ${temp_dir}/NNTraining_check/*.done 2>/dev/null | wc -l)
-    if [ "$done_count" -eq 1 ]; then
-      rm -rf ${temp_dir}/NNTraining_check/*.done
-      break
-    fi
-      sleep 1
-  done
-
-
-fi
+      fi
 
 
 
 
-if [[ "$j" != "${group}" ]]; then
+      if [[ "$j" != "${group}" ]]; then
 
-  echo "Starting next simulation batch"
+        echo "Starting next simulation batch"
 
-  next_group=$((j + 1))
+        next_group=$((j + 1))
 
-  for ((n = 0; n < Batch_Size; n++)); do
-    # Make text file to communicate with Simulation.sh
-    cat > start_Simulations_${n}.txt <<EOT
+        for ((n = 0; n < Batch_Size; n++)); do
+          # Make text files to communicate with Simulations.sh and start next batch
+          cat > start_Simulations_${n}.txt <<EOT
 particle=${particle}
 energy=${i}
 group=${next_group}
 SPAD_Size=${s}
 EOT
 
-    mv start_Simulations_${n}.txt ${temp_dir}/
+          mv start_Simulations_${n}.txt ${temp_dir}/
 
-  done
+        done
 
-  echo "Simulation batch  started"
+        echo "Simulation jobs started"
 
-  while true; do
-    done_count=$(ls ${temp_dir}/Simulation_check/*.done 2>/dev/null | wc -l)
-    if [ "$done_count" -eq "${Batch_Size}" ]; then
-      rm -rf ${temp_dir}/Simulation_check/*.done
-      break
-    fi
-      sleep 1
-  done
 
-  echo "Simulation jobs finished"
+        # Waiting for simulations to finish
+        while true; do
+          done_count=$(ls ${temp_dir}/Simulation_check/*.done 2>/dev/null | wc -l)
+          if [ "$done_count" -eq "${Batch_Size}" ]; then
+            rm -rf ${temp_dir}/Simulation_check/*.done
+            echo "Simulation jobs finished"
+            break
+          fi
+            sleep 1
+        done
 
-  while true; do
-    done_count=$(ls ${temp_dir}/NNTraining_check/*.done 2>/dev/null | wc -l)
-    if [ "$done_count" -eq 1 ]; then
-      rm -rf ${temp_dir}/NNTraining_check/*.done
-      break
-    fi
-      sleep 1
-  done
 
-  echo "Training jobs finished"
+        # Waiting for training to finish
+        while true; do
+          done_count=$(ls ${temp_dir}/NNTraining_check/*.done 2>/dev/null | wc -l)
+          if [ "$done_count" -eq 1 ]; then
+            rm -rf ${temp_dir}/NNTraining_check/*.done
+            echo "Training job finished"
+            break
+          fi
+            sleep 1
+        done
 
-  echo "Making tensors"
 
-  # Now run code to make tensors
 
-  for ((n = 0; n < Batch_Size; n++)); do
-    # Make text file to communicate with Simulation.sh
-    cat > start_tensorMaker_${n}.txt <<EOT
+        for ((n = 0; n < Batch_Size; n++)); do
+          # Make text files to communicate with mass_tensorMaker.sh and start next batch
+          cat > start_tensorMaker_${n}.txt <<EOT
 particle=${particle}
 energy=${i}
 group=${next_group}
 SPAD_Size=${s}
 EOT
 
-    mv start_tensorMaker_${n}.txt ${temp_dir}/
+          mv start_tensorMaker_${n}.txt ${temp_dir}/
+
+        done
+
+        echo "Tensor jobs started"
+
+
+        # Wait for tensor making to finish
+        while true; do
+          done_count=$(ls ${temp_dir}/tensorMaker_check/*.done 2>/dev/null | wc -l)
+          if [ "$done_count" -eq "${Batch_Size}" ]; then
+            rm -rf ${temp_dir}/tensorMaker_check/*.done
+            echo "Tensor jobs finished"
+            break
+          fi
+            sleep 1
+        done
+
+
+
+      fi
+
+
+      echo "Group ${j} in energy ${i} for SPAD size ${s} complete"
+
+    done
+
+
+    # Plot the losses throughout NN epochs
+    cd NNTraining/${s}_model
+    python3 -u plot_loss.py NN_model_${s}/loss_history_${i}.txt
+    cd ../..
+
+    echo "Energy ${i} for SPAD size ${s} complete"
 
   done
 
-  echo "Tensor jobs started"
+  cd output_${s}
 
+  # Creating time-sliced xy projected photon histograms
+  echo "Creating histogram for ${s} SPADs"
+  python3 -u ../create_histos.py \
+  summed_tensor_${s}/summed_tensor.npy \
+  ${s}
 
-  # Wait for tensor making to finish
-  while true; do
-    done_count=$(ls ${temp_dir}/tensorMaker_check/*.done 2>/dev/null | wc -l)
-    if [ "$done_count" -eq "${Batch_Size}" ]; then
-      rm -rf ${temp_dir}/tensorMaker_check/*.done
-      break
-    fi
-      sleep 1
-  done
+  cd ${home_dir}
 
-  echo "Tensor jobs finished"
-
-
-fi
-
-
-echo "Group ${j} in energy ${i} for SPAD size ${s} complete"
-
-done
-
-cd NNTraining/${s}_model
-python3 -u plot_loss.py NN_model_${s}/loss_history_${i}.txt
-cd ../..
-
-echo "Energy ${i} for SPAD size ${s} complete"
-
-done
-
-cd output_${s}
-
-# Creating histograms
-echo "Creating histogram for ${s} SPADs"
-python3 -u ../create_histos.py \
-summed_tensor_${s}/summed_tensor.npy \
-${s}
-
-cd ${home_dir}
-
-echo "SPAD size ${s} complete"
+  echo "SPAD size ${s} complete"
 
 done
 
 echo "masterTrain.sh finished"
 
-#####################################################
 EOF
 
     chmod +x "$script_name"
