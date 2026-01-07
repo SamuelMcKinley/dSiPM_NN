b7dee2ce95c439a322bbc1958a7fa15de1313297
Samuel McKinley <sjmck17@gmail.com>
2026-01-07 10:55:46 -0600

    Update file clearing

--- clear_files.sh
+++ clear_files.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 rm -rf LOGDIR/*
-rm -rf batch_jobs/LOGDIR/*
 rm -rf NNTraining/*0_model
-rm -rf Training_Outputs/*
+rm -rf Training_Outputs
 rm -rf /lustre/scratch/$USER/dSiPM_NN
 rm -rf *.csv
 rm -rf batch_jobs/*
 rm -rf tensor_meta_*
-
-
+rm -rf NN_photons_model*
+rm -rf plots
